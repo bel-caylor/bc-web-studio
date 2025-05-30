ce547cd5f3490182034358c4b1236294e3f3603c
Belinda Caylor <belinda.caylor@gmail.com>
2025-05-30 09:11:12 -0500

    Project Section

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("find", (arr, attr, value) =>
     arr.find(item => item[attr] === value)
   );
-  
+  eleventyConfig.addPassthroughCopy({ "src/img/logos": "img/logos" });
+  eleventyConfig.addPassthroughCopy({ "src/img/screenshots": "img/screenshots" });
 
   return {
     dir: {
