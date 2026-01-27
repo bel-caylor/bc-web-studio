76281820458210e89cda8d029d3e946b4dc67a17
Belinda Caylor <belinda.caylor@gmail.com>
2026-01-27 16:38:34 -0600

    Setup card and hero styles

--- .eleventy.js
+++ .eleventy.js
@@ -23,9 +23,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addWatchTarget("src/js");
   eleventyConfig.addWatchTarget("src/style.css");
-  eleventyConfig.addPassthroughCopy({ "src/img/logos": "img/logos" });
-  eleventyConfig.addPassthroughCopy({ "src/img/screenshots": "img/screenshots" });
-  eleventyConfig.addPassthroughCopy({ "src/img/hero-bg.png": "img/hero-bg.png" });
+  eleventyConfig.addPassthroughCopy({ "src/img": "img" });
 
   eleventyConfig.addWatchTarget("dist/style.css");
   eleventyConfig.addWatchTarget("dist/js");
